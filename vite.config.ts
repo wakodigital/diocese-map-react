ff165a68a7b3438aa13bce37927f827929ccb853
wakodigital <info@wakodigital.com>
2026-06-07 11:43:42 -0600

    removed headers

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,4 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/diocese-map-react/",
-  headers: {
-    'Cache-Control': 'no-cache, no-store, must-revalidate'
-  }
 });
