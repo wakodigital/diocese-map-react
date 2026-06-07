e25ffb0a139fcbddd190593d1b38356241dba289
wakodigital <info@wakodigital.com>
2026-06-07 11:41:31 -0600

    updated Yselta mission long lat, it was incorrect.

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/diocese-map-react/",
+  headers: {
+    'Cache-Control': 'no-cache, no-store, must-revalidate'
+  }
 });
